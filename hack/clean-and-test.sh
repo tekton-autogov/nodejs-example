fa372960a2873fb8c1fec2faf93ddd2e1c650f02
Dillon Winchell <dwinchel@redhat.com>
2022-12-01 15:59:04 -0500

    Update test script

--- hack/clean-and-test.sh
+++ hack/clean-and-test.sh
@@ -2,3 +2,6 @@ oc delete -f ./k8s/
 oc delete -f ./tekton/
 oc create -f ./tekton/
 oc create -f ./hack/pipelinerun.yaml
+watch tkn pipelinerun describe --last
+curl $(oc get route nodejs-example -o jsonpath='{.spec.host}') && echo
+
