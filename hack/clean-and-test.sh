7215eafef1d5d36afe6ab89172178b1214e5f69f
Dillon Winchell <dwinchel@redhat.com>
2022-12-02 16:46:59 -0500

    Change test script

--- hack/clean-and-test.sh
+++ hack/clean-and-test.sh
@@ -7,7 +7,7 @@ oc create -f ./tekton/
 
 # Test
 oc create -f ./hack/pipelinerun.yaml
-watch -b tkn pipelinerun describe --last # Watch description of pipeline until it completes
+watch tkn pipelinerun describe --last # Watch description of pipeline until it completes
 tkn pipelinerun describe --last # Print description of pipeline after watch exists so that it stays on screen
 curl -L $(oc get route nodejs-example -o jsonpath='{.spec.host}') && echo # Test the route
 
