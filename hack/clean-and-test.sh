0dc456f1917d4820c4a535864e9be5ffb2566f3c
Dillon Winchell <dwinchel@redhat.com>
2022-12-02 13:18:55 -0500

    Update test script

--- hack/clean-and-test.sh
+++ hack/clean-and-test.sh
@@ -1,7 +1,12 @@
+# Uninstall
 helm uninstall helm-release
 oc delete -f ./tekton/
+
+# Install
 oc create -f ./tekton/
+
+# Test
 oc create -f ./hack/pipelinerun.yaml
 watch tkn pipelinerun describe --last
-curl $(oc get route nodejs-example -o jsonpath='{.spec.host}') && echo
+curl -L $(oc get route nodejs-example -o jsonpath='{.spec.host}') && echo
 
