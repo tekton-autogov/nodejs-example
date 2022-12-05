3f97e5ef593020972e36a5c1108044fdd98c5eab
Dillon Winchell <dwinchel@redhat.com>
2022-12-05 01:10:35 -0500

    Start more pipelines in test script

--- hack/clean-and-test.sh
+++ hack/clean-and-test.sh
@@ -7,12 +7,9 @@ oc create -f ./tekton/
 
 # Test
 oc create -f ./dockerfile-quickstart/tekton/dockerfile-minimal-pipelinerun.yaml
-#oc create -f ./dockerfile-quickstart/tekton/dockerfile-acs-pipelinerun.yaml
-#oc create -f ./nodejs-quickstart/tekton/dockerfile-minimal-pipelinerun.yaml
-#oc create -f ./spring-boot-quickstart/tekton/maven-minimal-pipelinerun.yaml
-#oc create -f ./go-quickstart/tekton/dockerfile-minimal-pipelinerun.yaml
-#oc create -f ./hack/acs-pipelinerun.yaml
-#oc create -f ./hack/sigstore-pipelinerun.yaml
+oc create -f ./nodejs-quickstart/tekton/dockerfile-minimal-pipelinerun.yaml
+oc create -f ./spring-boot-quickstart/tekton/java-minimal-pipelinerun.yaml
+oc create -f ./go-quickstart/tekton/dockerfile-minimal-pipelinerun.yaml
 #watch tkn pipelinerun describe --last # Watch description of pipeline until it completes
 #curl -L $(oc get route dockerfile-quickstart -n dockerfile-quickstart -o jsonpath='{.spec.host}') && echo # Test the route
 
